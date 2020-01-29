801990b2ac88e04b4d1b0a7dcb399d7fe8c9fe2e
kqsantos <kqsantos@uw.edu>
2020-01-29 13:23:17 -0800

    added sidebar

--- Area51Script.js
+++ Area51Script.js
@@ -164,64 +164,90 @@ function ActivateAI() {
      */
 }
 
-function LoadResourceDisplay(game) {
-    function Background(game) {
-        Entity.call(this, game, 0, 400);
-        this.radius = 200;
-    }
-
-    Background.prototype = new Entity();
-    Background.prototype.constructor = Background;
-
-    Background.prototype.update = function () {
-    }
-
-    Background.prototype.draw = function (ctx) {
-        ctx.fillStyle = "SaddleBrown";
-        ctx.fillRect(0, 500, 800, 300);
-        Entity.prototype.draw.call(this);
-    }
+// === START OF RESOURCE DISPLAY ===
+function ResourceDisplay(game) {
+    Entity.call(this, game, 0, 400);
+    this.radius = 200;
 }
 
-function LoadBuildDisplay() {
-    //var canvas = document.getElementById("gameWorld");
-    //var ctx = canvas.getContext('2d');
+ResourceDisplay.prototype = new Entity();
+ResourceDisplay.prototype.constructor = ResourceDisplay;
 
+ResourceDisplay.prototype.update = function () {
+}
+
+ResourceDisplay.prototype.draw = function (ctx) {
     ctx.fillStyle = "#a7b0c2";
     ctx.strokeStyle = "black";
     ctx.fillRect(900, 0, 380, 50);
     ctx.strokeRect(900, 0, 380, 50);
+    Entity.prototype.draw.call(this);
+}
+// === END OF RESOURCE DISPLAY ===
 
+// === START OF BUILD DISPLAY ===
+function BuildDisplay(game) {
+    Entity.call(this, game, 0, 400);
+    this.radius = 200;
+}
+
+BuildDisplay.prototype = new Entity();
+BuildDisplay.prototype.constructor = ResourceDisplay;
+
+BuildDisplay.prototype.update = function () {
+}
+
+BuildDisplay.prototype.draw = function (ctx) {
     ctx.fillStyle = "#7e8594";
     ctx.strokeStyle = "black";
     ctx.fillRect(900, 50, 380, 310);
     ctx.strokeRect(900, 50, 380, 310);
+    Entity.prototype.draw.call(this);
+}
+// === END OF BUILD DISPLAY ===
 
+// === START OF MOVE DISPLAY ===
+function MoveDisplay(game) {
+    Entity.call(this, game, 0, 400);
+    this.radius = 200;
+}
+
+MoveDisplay.prototype = new Entity();
+MoveDisplay.prototype.constructor = ResourceDisplay;
+
+MoveDisplay.prototype.update = function () {
+}
+
+MoveDisplay.prototype.draw = function (ctx) {
     ctx.fillStyle = "#676d7a";
     ctx.strokeStyle = "black";
     ctx.fillRect(900, 360, 380, 310);
     ctx.strokeRect(900, 360, 380, 310);
+    Entity.prototype.draw.call(this);
+}
+// === END OF MOVE DISPLAY ===
 
+// === START OF ENDTURN DISPLAY ===
+function EndTurnDisplay(game) {
+    Entity.call(this, game, 0, 400);
+    this.radius = 200;
+}
+
+EndTurnDisplay.prototype = new Entity();
+EndTurnDisplay.prototype.constructor = ResourceDisplay;
+
+EndTurnDisplay.prototype.update = function () {
+}
+
+EndTurnDisplay.prototype.draw = function (ctx) {
     ctx.fillStyle = "#4d525c";
     ctx.strokeStyle = "black";
     ctx.fillRect(900, 670, 380, 50);
     ctx.strokeRect(900, 670, 380, 50);
-
-
-    // ctx.fillStyle = "Blue";
-    // ctx.strokeStyle = "Blue";
-    // ctx.beginPath();
-    // ctx.arc(50, 50, 25, 0, 2*Math.PI);
-    // ctx.fill();
-    // ctx.stroke();
-
-    // ctx.fillStyle = "Red";
-    // ctx.strokeStyle = "Red";
-    // ctx.beginPath();
-    // ctx.moveTo(100, 100);
-    // ctx.lineTo(200, 100);
-    // ctx.stroke();
+    Entity.prototype.draw.call(this);
 }
+// === END OF ENDTURN DISPLAY ===
+
 
 /**
  * This function will be onLoad from the canvas
@@ -235,9 +261,6 @@ function Main() {
      * attach GAME keyboard events
      * loadMap
      */
-
-    
-
     var ASSET_MANAGER = new AssetManager();
 
     ASSET_MANAGER.queueDownload("./img/food_icon.png");
@@ -248,158 +271,20 @@ function Main() {
         var ctx = canvas.getContext('2d');
 
         var gameEngine = new GameEngine();
-        var bg = new LoadResourceDisplay(gameEngine);
+        var resourceDP = new ResourceDisplay(gameEngine);
+        var buildDP = new BuildDisplay(gameEngine);
+        var moveDP = new MoveDisplay(gameEngine);
+        var endTurnDP = new EndTurnDisplay(gameEngine);
 
-        gameEngine.addEntity(bg);
+        gameEngine.addEntity(resourceDP);
+        gameEngine.addEntity(buildDP);
+        gameEngine.addEntity(moveDP);
+        gameEngine.addEntity(endTurnDP);
 
         gameEngine.init(ctx);
         gameEngine.start();
     });
 
-    //LoadBuildDisplay();
 }
 
-
-
 Main();
-
-
-
-
-
-
-// function Animation(spriteSheet, startX, startY, frameWidth, frameHeight, frameDuration, frames, loop, reverse) {
-//     this.spriteSheet = spriteSheet;
-//     this.startX = startX;
-//     this.startY = startY;
-//     this.frameWidth = frameWidth;
-//     this.frameDuration = frameDuration;
-//     this.frameHeight = frameHeight;
-//     this.frames = frames;
-//     this.totalTime = frameDuration * frames;
-//     this.elapsedTime = 0;
-//     this.loop = loop;
-//     this.reverse = reverse;
-// }
-
-// Animation.prototype.drawFrame = function (tick, ctx, x, y, scaleBy) {
-//     var scaleBy = scaleBy || 1;
-//     this.elapsedTime += tick;
-//     if (this.loop) {
-//         if (this.isDone()) {
-//             this.elapsedTime = 0;
-//         }
-//     } else if (this.isDone()) {
-//         return;
-//     }
-//     var index = this.reverse ? this.frames - this.currentFrame() - 1 : this.currentFrame();
-//     var vindex = 0;
-//     if ((index + 1) * this.frameWidth + this.startX > this.spriteSheet.width) {
-//         index -= Math.floor((this.spriteSheet.width - this.startX) / this.frameWidth);
-//         vindex++;
-//     }
-//     while ((index + 1) * this.frameWidth > this.spriteSheet.width) {
-//         index -= Math.floor(this.spriteSheet.width / this.frameWidth);
-//         vindex++;
-//     }
-
-//     var locX = x;
-//     var locY = y;
-//     var offset = vindex === 0 ? this.startX : 0;
-//     ctx.drawImage(this.spriteSheet,
-//                   index * this.frameWidth + offset, vindex * this.frameHeight + this.startY,  // source from sheet
-//                   this.frameWidth, this.frameHeight,
-//                   locX, locY,
-//                   this.frameWidth * scaleBy,
-//                   this.frameHeight * scaleBy);
-// }
-
-// Animation.prototype.currentFrame = function () {
-//     return Math.floor(this.elapsedTime / this.frameDuration);
-// }
-
-// Animation.prototype.isDone = function () {
-//     return (this.elapsedTime >= this.totalTime);
-// }
-
-// function Background(game) {
-//     Entity.call(this, game, 0, 400);
-//     this.radius = 200;
-// }
-
-// Background.prototype = new Entity();
-// Background.prototype.constructor = Background;
-
-// Background.prototype.update = function () {
-// }
-
-// Background.prototype.draw = function (ctx) {
-//     ctx.fillStyle = "SaddleBrown";
-//     ctx.fillRect(0,500,800,300);
-//     Entity.prototype.draw.call(this);
-// }
-
-// function Unicorn(game) {
-//     this.animation = new Animation(ASSET_MANAGER.getAsset("./img/RobotUnicorn.png"), 0, 0, 206, 110, 0.02, 30, true, true);
-//     this.jumpAnimation = new Animation(ASSET_MANAGER.getAsset("./img/RobotUnicorn.png"), 618, 334, 174, 138, 0.02, 40, false, true);
-//     this.jumping = false;
-//     this.radius = 100;
-//     this.ground = 400;
-//     Entity.call(this, game, 0, 400);
-// }
-
-// Unicorn.prototype = new Entity();
-// Unicorn.prototype.constructor = Unicorn;
-
-// Unicorn.prototype.update = function () {
-//     if (this.game.space) this.jumping = true;
-//     if (this.jumping) {
-//         if (this.jumpAnimation.isDone()) {
-//             this.jumpAnimation.elapsedTime = 0;
-//             this.jumping = false;
-//         }
-//         var jumpDistance = this.jumpAnimation.elapsedTime / this.jumpAnimation.totalTime;
-//         var totalHeight = 200;
-
-//         if (jumpDistance > 0.5)
-//             jumpDistance = 1 - jumpDistance;
-
-//         //var height = jumpDistance * 2 * totalHeight;
-//         var height = totalHeight*(-4 * (jumpDistance * jumpDistance - jumpDistance));
-//         this.y = this.ground - height;
-//     }
-//     Entity.prototype.update.call(this);
-// }
-
-// Unicorn.prototype.draw = function (ctx) {
-//     if (this.jumping) {
-//         this.jumpAnimation.drawFrame(this.game.clockTick, ctx, this.x + 17, this.y - 34);
-//     }
-//     else {
-//         this.animation.drawFrame(this.game.clockTick, ctx, this.x, this.y);
-//     }
-//     Entity.prototype.draw.call(this);
-// }
-
-// // the "main" code begins here
-
-// var ASSET_MANAGER = new AssetManager();
-
-// //ASSET_MANAGER.queueDownload();
-// ASSET_MANAGER.downloadAll();
-
-// ASSET_MANAGER.downloadAll(function () {
-//     console.log("starting up da sheild");
-//     var canvas = document.getElementById('gameWorld');
-//     var ctx = canvas.getContext('2d');
-
-//     var gameEngine = new GameEngine();
-//     var bg = new Background(gameEngine);
-//     //var unicorn = new Unicorn(gameEngine);
-
-//     gameEngine.addEntity(bg);
-//     //gameEngine.addEntity(unicorn);
-
-//     gameEngine.init(ctx);
-//     gameEngine.start();
-// });
